e2b1014bb26131586cb39d80596963c3360d56e0
scrappy1987 <jagordon87@msn.com>
2018-07-17 17:55:09 +0100

    refactored code

--- src/main/java/com/qa/constants/Constants.java
+++ src/main/java/com/qa/constants/Constants.java
@@ -3,4 +3,6 @@ package com.qa.constants;
 public interface Constants {
 
 	static final String API_ADDRESS = "http://localhost:8080/trainer/getAllTrainers";
+	static final String TOPIC_EXCHANGE_NAME = "spring-boot-exchange";
+	static final String QUEUE_NAME = "spring-boot";
 }
