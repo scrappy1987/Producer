0663c751dca05ef33b7d6acfb183ad1ba056c3bb
scrappy1987 <jagordon87@msn.com>
2018-07-17 15:14:26 +0100

    next commit

--- src/main/java/com/qa/Application.java
+++ src/main/java/com/qa/Application.java
@@ -2,43 +2,55 @@ package com.qa;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 import com.google.gson.Gson;
-import com.qa.constants.Constants;
-import com.qa.domain.Trainer;
 
 @SpringBootApplication
 public class Application {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
+	
+	static final String topicExchangeName = "spring-boot-exchange";
 
-	public static void main(String args[]) {
-		SpringApplication.run(Application.class);
+	static final String queueName = "spring-boot";
+
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange(topicExchangeName);
+	}
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
 	}
 	
 	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
+	RestTemplate getRestTemplate() 
+	{
+		return new RestTemplate();
 	}
 	
 	@Bean
-	public Gson gson() {
+	Gson getGson() 
+	{
 		return new Gson();
 	}
 
-	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return args -> {
-			Trainer[] trainer = restTemplate.getForObject(
-					Constants.API_ADDRESS, Trainer[].class);
-			String gsonString = gson().toJson(trainer);
-			log.info("This is the value of the API call " + gsonString);
-		};
+	public static void main(String args[]) {
+		SpringApplication.run(Application.class).close();;
 	}
+	
 }
---
+++ src/main/java/com/qa/Runner.java
@@ -0,0 +1,34 @@
+package com.qa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.google.gson.Gson;
+import com.qa.constants.Constants;
+import com.qa.domain.Trainer;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+	private final RabbitTemplate rabbitTemplate;
+	private final RestTemplate restTemplate;
+	private final Gson gson;
+
+    public Runner(RabbitTemplate rabbitTemplate, RestTemplate restTemplate, Gson gson) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.restTemplate = restTemplate;
+        this.gson = gson;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    	Trainer[] trainer = restTemplate.getForObject(
+				Constants.API_ADDRESS, Trainer[].class);
+        rabbitTemplate.convertAndSend(Application.topicExchangeName, "foo.bar.baz", gson.toJson(trainer));
+    }
+}
